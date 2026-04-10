84ca140249fd6521ae1035c916a69726d60d5053
ThisRaihann <mr.fewsx212@gmail.com>
2026-04-10 13:25:36 +0700

    eror

--- src/content/content.config.ts
+++ src/content/content.config.ts
@@ -1,18 +1,19 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: 'content',
+const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.coerce.date(), 
+    pubDate: z.string(),
     tags: z.array(z.string()),
     image: z.object({
       url: z.string(),
       alt: z.string(),
-    }).optional(),
+    }),
     readingTime: z.number().optional(),
   }),
 });
 
-export const collections = { 'blog': blog };
+export const collections = {
+  blog: blogCollection,
+};
